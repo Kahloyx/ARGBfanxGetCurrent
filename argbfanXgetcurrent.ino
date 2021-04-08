89a7089dbafea4bab972caf5fab68d6d5dac8ca3
Kahloyx <44975509+Kahloyx@users.noreply.github.com>
2021-04-09 00:00:30 +0200

    Update argbfanXgetcurrent.ino

--- argbfanXgetcurrent.ino
+++ argbfanXgetcurrent.ino
@@ -1,18 +1,21 @@
+/*	This sketch is just a modification of an existing sketch that I found on internet, I kept it but I can't remind where I originally found it.
+*/
+
 #include <Wire.h>
 #include <Adafruit_INA219.h>
 
-Adafruit_INA219 ina219;    // Have introduced the ina 219 part, lets make the rest
+Adafruit_INA219 ina219;    // Have introduced the ina 219 part
 
 #include <FastLED.h>
-int argbSwitch = 2;    // This pin is used to feed the argb ring with 5V
-int currpal = 4;        // This one is the palette by default (the aestethicc one)
-int maxPalette = 4;     // This is the number of registrered palette we got
+int argbSwitch = 2;    // This pin is used to power the argb led ring of the desktop PC fan
+int currpal = 4;        // This one is the palette by default (FouthPalette())
+int maxPalette = 4;     // This is the number of registrered palette I got into the sketch
 
-#define LED_PIN     5       // This paragraph depends on your fan tho
-#define NUM_LEDS    14
-#define BRIGHTNESS  64
-#define LED_TYPE    WS2811
-#define COLOR_ORDER GRB
+#define LED_PIN     5       // The data pin of the argb led strip
+#define NUM_LEDS    14		//Ofc it depends of your led strip
+#define BRIGHTNESS  64		// Decimal value between 0 and 255...
+#define LED_TYPE    WS2811	// Ofc it depends of your led strip
+#define COLOR_ORDER GRB		// Same ...
 CRGB leds[NUM_LEDS];
 
 #define UPDATES_PER_SECOND 60 // I used 42 for the secondPalette() 2520/42=60
@@ -22,7 +25,7 @@ TBlendType    currentBlending;
 
 extern CRGBPalette16 myRedWhiteBluePalette;
 extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
-int i = 0; //This one is gonna be the one who tell when we mesure currentflow, so each 200 leds cycles
+int i = 0; //This int is just giving me a start for measuring with the INA219 once it reaches 200 cycles (In this precise sketch)
 
 void setup() {
   delay( 200 ); // power-up safety delay
@@ -38,7 +41,7 @@ void setup() {
   }
 
   uint32_t currentFrequency;
-	pinMode(argbSwitch, OUTPUT);  // The alimentation of the argb ring onto the fan
+	pinMode(argbSwitch, OUTPUT);  // This brings power on the + pin of the argb led strip
   int j = 0;
   while (j < 2) {               // Just an init as we love'em
     fill_solid( currentPalette, 16, CRGB::White);
@@ -46,7 +49,7 @@ void setup() {
     startIndex = startIndex + 1; /* motion speed */
     FillLEDsFromPaletteColors( startIndex);
     FastLED.show();
-	  digitalWrite(argbSwitch, HIGH);  // By activating this output we feed the Vin of the color ring and so it light up
+	  digitalWrite(argbSwitch, HIGH);
     FastLED.delay(5040 / UPDATES_PER_SECOND);
     digitalWrite(argbSwitch, LOW);   // And then we shut it off, init thing ya know
     delay(420);
@@ -93,7 +96,7 @@ void loop() {
   if (currpal == 4){
     FourthPalette();
   }
-  if (currpal > maxPalette){    // This way we cycle throught the palette we got
+  if (currpal > maxPalette){    // This way we cycle throught the palette we got			/* Spaghetti code but I can't find better option ATM */
     currpal = 1;
   }
 
@@ -108,15 +111,15 @@ void loop() {
 
   if ( Serial.available()) {     // Check if any char available on serial, if there's any instructions
     char c = Serial.read();
-    if (c == 's'){
+    if (c == 's'){			// Order to change to the next palette
       Serial.println("Switching.");
       Serial.println(c);
       currpal = currpal +1;
     }
-    if (c == 'n'){
+if (c == 'n'){				// Order to power on the argb led strip 
       digitalWrite(argbSwitch, HIGH);
     }
-    if (c == 'f'){
+    if (c == 'f'){			// Order to power off the argb led strip
       digitalWrite(argbSwitch, LOW);
     }
   }
